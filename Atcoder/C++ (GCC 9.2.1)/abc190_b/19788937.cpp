29ec2eea834ab667c4f28b1e0311a9aa0a518721
ayushranjan26 <56040311+ayushranjan26@users.noreply.github.com>
2022-07-18 16:59:50 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc190_b/19788937.cpp
@@ -0,0 +1,84 @@
+	/*WELCOME TO AYUSH RANJAN CODE'S*/
+/*EVERY PROBLEM HAS A SOLUTION SOMETIMES ELEGENT OTHERWISE COMPLEX ELEGENCE*/
+
+#include <bits/stdc++.h>
+#define ll long long int
+#define IOS cin.tie(NULL);cout.tie(NULL)
+using namespace std;
+
+const int mod = 1000000007;
+
+int chkpower2(int x){
+    return (x&&!(x&x-1));
+}
+vector<ll> primeFactors(ll n)
+{
+    vector<ll> v;
+    while (n%2 == 0)
+    {
+        v.push_back(2);
+        n = n/2;
+    }
+    for (ll i = 3; i <= sqrt(n); i = i+2)
+    {
+        while (n%i == 0)
+        {
+            v.push_back(i);
+            n = n/i;
+        }
+    }
+    v.push_back(n);
+    return v;
+}
+int freq[3];
+void solve(){
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i1=0;i1<n;i1++){
+        cin>>a[i1];
+    }
+    ll prefix_sum[n];
+    ll max_pre_sum[n];
+    prefix_sum[0]=a[0];
+    for(int i1=1;i1<n;i1++){
+        prefix_sum[i1]=prefix_sum[i1-1]+a[i1];
+    }
+    max_pre_sum[0]=prefix_sum[0];
+    for(int i1=1;i1<n;i1++){
+        max_pre_sum[i1]=max(max_pre_sum[i1-1],prefix_sum[i1]);
+    }
+    ll pre_sum[n];
+    pre_sum[0]=prefix_sum[0];
+    for(int i1=1;i1<n;i1++){
+        pre_sum[i1]=pre_sum[i1-1]+prefix_sum[i1];
+    }
+
+    for(int i1=0;i1<n;i1++){
+        //cout<<pre_sum[i1]<<" "<<max_pre_sum[i1]<<endl;
+    }
+
+    ll ans[n];
+    ans[0]=prefix_sum[0];
+    for(int i1=1;i1<n;i1++){
+        if(i1==n-1){
+            ans[i1]=max(ans[i1-1],pre_sum[i1]);
+            continue;
+        }
+        ans[i1]=max(ans[i1-1],max(pre_sum[i1],pre_sum[i1]+max_pre_sum[i1]));
+       // cout<<ans[i1]<<" ";
+    }
+    cout<<max(0ll,ans[n-1]);
+    return;
+}
+int main()
+{
+    //IOS;
+    int t;
+    t=1;//cin>>t;
+    while(t--){
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
