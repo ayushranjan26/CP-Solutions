b09a55dd50b790f2b216b9b5e9bee7a0bb7c7c93
ayushranjan26 <56040311+ayushranjan26@users.noreply.github.com>
2022-07-18 16:51:52 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc256_a/32538811.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+#define int long long int
+#define IOS std::ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define ft first
+#define sd second
+#define pii pair<int,int>
+
+using namespace std;
+
+const int mod = 1e9+7;
+
+void solve(){
+    int i,j,n;
+    cin>>n;
+    int res=1;
+    for(i=0;i<n;i++){
+        res=res*2;
+    }
+    cout<<res;
+    return;
+}
+
+int32_t main(){
+    //IOS;
+    int t,tc=1;
+    t=1;
+    //cin>>t;
+    while(tc<=t){
+        solve();
+        cout<<"\n";
+        tc++;
+    }
+    return 0;
+}
