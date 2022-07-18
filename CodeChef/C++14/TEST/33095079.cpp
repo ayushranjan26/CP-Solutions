311177d8cc65d6e7b9eb1722151233cbd29b4bf8
ayushranjan26 <56040311+ayushranjan26@users.noreply.github.com>
2022-07-18 17:02:29 +0530

    Committing files

---
+++ CodeChef/C++14/TEST/33095079.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    while(cin>>n){
+        if(n==42){
+            break;
+        }
+        else{
+            cout<<n<<endl;
+        }
+    }
+    return 0;
+}
