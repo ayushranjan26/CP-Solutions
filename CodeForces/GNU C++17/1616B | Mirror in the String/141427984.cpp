2ebe30dad3cc96814383dced722bb0916da7649a
ayushranjan26 <56040311+ayushranjan26@users.noreply.github.com>
2022-07-18 17:37:31 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1616B | Mirror in the String/141427984.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+ 
+#define int long long int
+#define IOS std::ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define ft first
+#define sd second
+#define pii pair<int,int>
+ 
+using namespace std;
+ 
+//---------------------- PROVE, THEN WRITE ----------------------//
+ 
+const int mod = 1e9+7;
+ 
+void solve(){
+    int i,j,n;
+    cin>>n;
+    string s;
+    cin>>s;
+    if(n==1||s[0]<=s[1]){
+        cout<<s[0]<<s[0];
+        return;
+    }
+    string res="";
+    res+=s[0];
+    for(i=1;i<n;i++){
+        if(s[i]<=s[i-1]){
+            res+=s[i];
+        }
+        else{
+            break;
+        }
+    }
+    cout<<res;
+    reverse(res.begin(),res.end());
+    cout<<res;
+    return;
+}
+ 
+int32_t main(){
+    //IOS;
+    int t,tc=1;
+    t=1;
+    cin>>t;
+    while(tc<=t){
+        solve();
+        cout<<"\n";
+        tc++;
+    }
+    return 0;
+}
