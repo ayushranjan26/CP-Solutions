25d36a2ae4e0c0fa1173e3e0b15a307df5683ea5
ayushranjan26 <56040311+ayushranjan26@users.noreply.github.com>
2022-07-18 17:47:19 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1606A | AB Balance/133510628.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+ 
+#define ll long long int
+#define IOS std::ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define ft first
+#define sd second
+#define pii pair<int,int>
+ 
+using namespace std;
+ 
+//---------------------- PROVE, THEN WRITE ----------------------//
+ 
+const int mod = 1e9+7;
+ 
+void solve(){
+    int i,j,n;
+    string s;
+    cin>>s;
+    n=s.size();
+    if(s[0]==s[n-1]){
+        cout<<s;
+    }
+    else{
+        s[n-1]=s[0];
+        cout<<s;
+    }
+    return;
+}
+ 
+int32_t main(){
+    //IOS;
+    int t,tc=1;
+    t=1;
+    cin>>t;
+    while(tc<=t){
+        solve();
+        cout<<"\n";
+        tc++;
+    }
+    return 0;
+}
