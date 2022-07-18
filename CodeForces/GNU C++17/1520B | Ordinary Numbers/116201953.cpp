cca3ee019cfab09c99e6a6d3ed3e284ca82e8324
ayushranjan26 <56040311+ayushranjan26@users.noreply.github.com>
2022-07-18 17:59:17 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1520B | Ordinary Numbers/116201953.cpp
@@ -0,0 +1,47 @@
+/*WELCOME TO AYUSH RANJAN CODE*/
+ 
+#include <bits/stdc++.h>
+ 
+#define ll long long int
+#define pb push_back
+#define IOS std::ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define all(x) (x).begin(), (x).end()
+#define loop(i,a,b) for(int i=(a);i<(b);i++)
+#define rloop(i,a,b) for(int i=(a);i>(b);i--)
+#define ft first
+#define sd second
+ 
+using namespace std;
+ 
+ll const INF =1e18;
+ll M =1000000007;
+ 
+void solve()
+{
+    ll i,j,k,n;
+    cin>>n;
+    ll ans=0;
+    for(i=1;i<10;i++){
+        ll d=i;
+        while(d<=n){
+            ans++;
+            d=d*10+i;
+        }
+    }
+    cout<<ans;
+    return;
+}
+ 
+int main()
+ 
+{
+    //IOS;
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
