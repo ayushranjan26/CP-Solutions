c5a0cb4f16ba08e6e43837b0e5b441a04ca83997
ayushranjan26 <56040311+ayushranjan26@users.noreply.github.com>
2022-07-18 18:01:44 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1505A | Is it rated - 2/111657600.cpp
@@ -0,0 +1,48 @@
+/*WELCOME TO AYUSH RANJAN CODE*/
+ 
+#include <bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define IOS std::ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define all(x) (x).begin(), (x).end()
+#define loop(i,a,b) for(int i=(a);i<(b);i++)
+#define rloop(i,a,b) for(int i=(a);i>(b);i--)
+#define ft first
+#define sd second
+ 
+using namespace std;
+ 
+ll const INF =1e18;
+ 
+ 
+void solve()
+{
+    ll i,j,k,n,m;
+    string x,y,z;
+    cin>>x>>y>>z;
+ 
+    return ;
+}
+ 
+int main()
+{
+    //IOS;
+    ll t;
+    string s;
+    while(1){
+    getline(cin,s);
+    if(s.size()){
+        cout<<"NO"<<endl;
+    }
+    else{
+        break;
+    }
+    }
+    t=0;//cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
