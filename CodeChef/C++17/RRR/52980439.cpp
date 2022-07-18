4bc2afd0d4d62bf88efabb498a1418b5a43b74ae
ayushranjan26 <56040311+ayushranjan26@users.noreply.github.com>
2022-07-18 17:01:39 +0530

    Committing files

---
+++ CodeChef/C++17/RRR/52980439.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+#define int long long
+#define IOS std::ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define ft first
+#define sd second
+#define pii pair<int,int>
+
+using namespace std;
+
+//---------------------- PROVE, THEN WRITE ----------------------//
+
+const int mod = 1e9+7;
+
+void solve(){
+    int i,j,k,n;
+    cin>>n>>k;
+    int res=n-k;
+    res+=(k-1)/2;
+    res=res*2;
+    cout<<res;
+    return;
+}
+
+int32_t main(){
+    //IOS;
+    int t,tc=1;
+    t=1;
+    cin>>t;
+    while(tc<=t){
+        solve();
+        cout<<"\n";
+        tc++;
+    }
+    return 0;
+}
