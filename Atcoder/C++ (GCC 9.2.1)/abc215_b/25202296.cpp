30013c44bd11e32307f213ef820ea0f95779b8f8
ayushranjan26 <56040311+ayushranjan26@users.noreply.github.com>
2022-07-18 16:59:01 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc215_b/25202296.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define int long long
+#define IOS std::ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define ft first
+#define sd second
+
+using namespace std;
+
+//---------------------- PROVE, THEN WRITE ----------------------//
+const int mod=1e9+7;
+const int N=1e5+6;
+
+
+void solve(){
+    int i,j,k,n,q;
+    string s;
+    cin>>s;
+    if(s=="Hello,World!"){
+        cout<<"AC";
+    }
+    else{
+        cout<<"WA";
+    }
+    return;
+}
+
+int32_t main(){
+    //IOS;
+    int t,tc=1;
+    t=1;
+    //cin>>t;
+    while(tc<=t){
+        solve();
+        cout<<"\n";
+        tc++;
+    }
+    return 0;
+}
