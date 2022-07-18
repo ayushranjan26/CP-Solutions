2846d6da5daa2093fa1c98ce74402f11cf3eda43
ayushranjan26 <56040311+ayushranjan26@users.noreply.github.com>
2022-07-18 18:14:26 +0530

    Committing files

---
+++ CodeForces/GNU C++17/913B | Christmas Spruce/89779211.cpp
@@ -0,0 +1,53 @@
+/*WELCOME TO AYUSH RANJAN CODE'S*/
+/*EVERY PROBLEM HAS A SOLUTION SOMETIMES ELEGENT OTHERWISE COMPLEX ELEGENCE*/
+ 
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+ 
+const int mod = 1e9 + 7;
+ 
+int chkpower2(int x){
+    return (x&&!(x&x-1));
+}
+ 
+ 
+void solve(){
+    int n,d;
+    cin>>n;
+    int a[n]={0};
+    int cnt[n]={0};
+    for(int i1=1;i1<n;i1++){
+        cin>>a[i1];
+        a[i1]--;
+        cnt[a[i1]]++;
+    }
+    int p[n]={0};
+    for(int i1=0;i1<n;i1++){
+        if(cnt[i1]==0){
+            p[a[i1]]++;
+        }
+    }
+    for(int i1=0;i1<n;i1++){
+        if(cnt[i1]>0&&p[i1]<3){
+            cout<<"NO";
+            return;
+        }
+    }
+    cout<<"YES";
+    return;
+}
+ 
+int main()
+{
+ //   ios::sync_with_stdio(0);
+  //  cin.tie(0);
+ 
+    int t;
+    t=1;//cin>>t;
+    while(t--){
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
