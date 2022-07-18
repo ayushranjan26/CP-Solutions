62e3ac839130f90758fcb9f465dfa0078c7a1dfe
ayushranjan26 <56040311+ayushranjan26@users.noreply.github.com>
2022-07-18 17:00:23 +0530

    Committing files

---
+++ CodeChef/C++17/ACS/67580614.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+#define int long long int
+#define IOS std::ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define ft first
+#define sd second
+#define pii pair<int,int>
+
+using namespace std;
+
+const int mod = 1e9+7;
+
+void solve(){
+    int i,j,n;
+    cin>>n;
+    int a=n/100+n%100;
+    if(a>10){
+        cout<<"-1";
+    }
+    else{
+        cout<<a;
+    }
+    return;
+}
+
+int32_t main(){
+    //IOS;
+    int t,tc=1;
+    t=1;
+    cin>>t;
+    while(tc<=t){
+        solve();
+        cout<<"\n";
+        tc++;
+    }
+    return 0;
+}
