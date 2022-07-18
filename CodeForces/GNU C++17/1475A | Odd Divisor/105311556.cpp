0b22b8969bbeb2ee36cde6dc78740a6ae521dc83
ayushranjan26 <56040311+ayushranjan26@users.noreply.github.com>
2022-07-18 18:06:31 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1475A | Odd Divisor/105311556.cpp
@@ -0,0 +1,131 @@
+/*WELCOME TO AYUSH RANJAN CODE'S*/
+ 
+#include <bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define endl "\n"
+#define IOS std::ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define all(x) (x).begin(), (x).end()
+#define loop(i,a,b) for(int i=(a);i<(b);i++)
+#define rloop(i,a,b) for(int i=(a);i>(b);i--)
+#define ft first
+#define sd second
+ 
+using namespace std;
+ 
+const ll mod = 1e9+7;
+const ll INF = 1e18;
+const ll MAX = 1e6+1;
+ 
+bool check_prime(ll n)
+{
+    ll i;
+    if(n<=1)
+    {
+        return false;
+    }
+    if(n==2)
+    {
+        return true;
+    }
+    if(n%2==0)
+    {
+        return false;
+    }
+    ll sn=sqrt(n);
+    for(i=3;i<=sn;i++){
+        if(n%i==0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+vector<bool> seive(ll n)
+{
+    ll i,j;
+    vector<bool> prime(n+1);
+    for(i=0;i<=n;i++)
+    {
+        prime[i]=true;
+    }
+    prime[0]=false;
+    prime[1]=false;
+    ll sn=sqrt(n);
+    for(i=2;i<=sn;i++)
+    {
+        if(prime[i])
+        {
+            for(j=i*i;j<=n;j+=i)
+            {
+                prime[j]=false;
+            }
+        }
+ 
+    }
+    return prime;
+}
+ll GCD(ll a,ll b)
+{
+    if(b==0)
+    {
+        return a;
+    }
+    return GCD(b,a%b);
+}
+vector<ll> primeFactors(ll n)
+{
+    vector<ll> v;
+    while (n%2 == 0)
+    {
+        v.push_back(2);
+        n = n/2;
+ 
+    for (ll i = 3; i <= sqrt(n); i = i+2)
+    {
+        while (n%i == 0)
+        {
+            v.push_back(i);
+            n = n/i;
+        }
+    }
+    v.push_back(n);
+    return v;
+    }
+}
+ 
+void solve()
+{
+    ll i,j,n,k,m,d,l,r;
+    cin>>n;
+    while(1){
+        if(n%2==0){
+            n/=2;
+        }
+        else{
+            if(n==1){
+                cout<<"NO";
+                return;
+            }
+            else{
+                cout<<"YES";
+                return;
+            }
+        }
+    }
+    return;
+ 
+}
+ 
+int main()
+{
+    //IOS;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
