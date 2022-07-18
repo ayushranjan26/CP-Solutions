f36bd30aa058bd6bdc86e8edcd6f89a2365c1cb3
ayushranjan26 <56040311+ayushranjan26@users.noreply.github.com>
2022-07-18 17:00:48 +0530

    Committing files

---
+++ CodeChef/C++17/BREAKSTICK/66094234.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+#define int long long int
+#define IOS std::ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define ft first
+#define sd second
+#define pii pair<int,int>
+
+using namespace std;
+
+const int mod = 998244353;
+
+void solve(){
+    int i,j,n,x;
+    cin>>n>>x;
+    if(n%2==0){
+        cout<<"YES";
+    }
+    else{
+        if(x%2==0){
+            cout<<"NO";
+        }
+        else{
+            cout<<"YES";
+        }
+    }
+    return;
+}
+
+int32_t main(){
+    //IOS;
+    int t,tc=1;
+    t=1;
+    cin>>t;
+    while(tc<=t){
+        solve();
+        cout<<"\n";
+        tc++;
+    }
+    return 0;
+}
