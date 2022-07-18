c9fd8984b9c85d5d118605cd1849dc9d7e89f2c9
ayushranjan26 <56040311+ayushranjan26@users.noreply.github.com>
2022-07-18 17:42:12 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1620A | Equal or Not Equal/139882354.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+ 
+#define int long long int
+#define IOS std::ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define ft first
+#define sd second
+#define pii pair<int,int>
+ 
+using namespace std;
+ 
+//---------------------- PROVE, THEN WRITE ----------------------//
+ 
+const int mod = 1e9+7;
+ 
+void solve(){
+    int i,j,k;
+    string s;
+    cin>>s;
+    int c=0;
+    for(i=0;i<s.size();i++){
+        if(s[i]=='N'){
+            c++;
+        }
+    }
+    if(c==1){
+        cout<<"NO";
+    }
+    else{
+        cout<<"YES";
+    }
+    return;
+}
+ 
+int32_t main(){
+    //IOS;
+    int t,tc=1;
+    t=1;
+    cin>>t;
+    while(tc<=t){
+        solve();
+        cout<<"\n\n";
+        tc++;
+    }
+    return 0;
+}
