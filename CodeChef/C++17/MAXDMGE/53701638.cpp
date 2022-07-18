6078e0147598a77d19008df0f8c5030cee55f7c8
ayushranjan26 <56040311+ayushranjan26@users.noreply.github.com>
2022-07-18 17:34:11 +0530

    Committing files

---
+++ CodeChef/C++17/MAXDMGE/53701638.cpp
@@ -0,0 +1,51 @@
+ #include <bits/stdc++.h>
+
+#define int long long
+#define IOS std::ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define ft first
+#define sd second
+#define pii pair<int,int>
+
+using namespace std;
+
+//---------------------- PROVE, THEN WRITE ----------------------//
+
+const int mod = 1e9+7;
+
+void solve(){
+    int i,j,n;
+    cin>>n;
+    int a[n];
+    for(i=0;i<n;i++){
+        cin>>a[i];
+    }
+    int ans[n];
+    for(i=0;i<n;i++){
+        if(i==0){
+            ans[i]=a[i]&a[i+1];
+        }
+        else if(i==n-1){
+            ans[i]=a[i]&a[i-1];
+        }
+        else{
+            ans[i]=max(a[i]&a[i+1],a[i]&a[i-1]);
+        }
+    }
+    for(i=0;i<n;i++){
+        cout<<ans[i]<<" ";
+    }
+    return;
+}
+
+int32_t main(){
+    //IOS;
+    int t,tc=1;
+    t=1;
+    cin>>t;
+    while(tc<=t){
+        solve();
+        cout<<"\n";
+        tc++;
+    }
+    return 0;
+}
