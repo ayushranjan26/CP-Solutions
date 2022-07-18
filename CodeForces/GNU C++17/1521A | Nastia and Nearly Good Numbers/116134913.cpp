ec928f897fd84e717ff2565e48a946983ae160fb
ayushranjan26 <56040311+ayushranjan26@users.noreply.github.com>
2022-07-18 17:59:27 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1521A | Nastia and Nearly Good Numbers/116134913.cpp
@@ -0,0 +1,48 @@
+/*WELCOME TO AYUSH RANJAN CODE*/
+ 
+#include <bits/stdc++.h>
+ 
+#define ll long long int
+#define pb push_back
+#define IOS std::ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define all(x) (x).begin(), (x).end()
+#define loop(i,a,b) for(int i=(a);i<(b);i++)
+#define rloop(i,a,b) for(int i=(a);i>(b);i--)
+#define ft first
+#define sd second
+ 
+using namespace std;
+ 
+ll const INF =1e18;
+ll M =1000000007;
+ 
+void solve()
+{
+    ll i,j,k,a,b;
+    cin>>a>>b;
+    if(b==1){
+        cout<<"NO";
+        return;
+    }
+    cout<<"YES\n";
+    if(b==2){
+        cout<<a<<" "<<a*3<<" "<<a*b*2;
+        return;
+    }
+    cout<<a*(b-1)<<" "<<a<<" "<<a*b;
+    return;
+}
+ 
+int main()
+ 
+{
+    //IOS;
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
