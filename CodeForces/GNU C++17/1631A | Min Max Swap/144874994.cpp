230c67973e110577d29b21960ed8304459bcabf3
ayushranjan26 <56040311+ayushranjan26@users.noreply.github.com>
2022-07-18 17:36:14 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1631A | Min Max Swap/144874994.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+ 
+#define int long long int
+#define IOS std::ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define ft first
+#define sd second
+#define pii pair<int,int>
+ 
+using namespace std;
+ 
+//---------------------- PROVE, THEN WRITE ----------------------//
+ 
+const int mod = 1e9+7;
+ 
+void solve(){
+    int i,j,n;
+    cin>>n;
+    int a[n],b[n];
+    for(i=0;i<n;i++){
+        cin>>a[i];
+    }
+    for(i=0;i<n;i++){
+        cin>>b[i];
+    }
+    for(i=0;i<n;i++){
+        if(a[i]>b[i]){
+            swap(a[i],b[i]);
+        }
+    }
+    int m1=0,m2=0;
+    for(i=0;i<n;i++){
+        m1=max(m1,a[i]);
+        m2=max(m2,b[i]);
+    }
+    cout<<m1*m2;
+    return;
+}
+ 
+int32_t main(){
+    //IOS;
+    int t,tc=1;
+    t=1;
+    cin>>t;
+    while(tc<=t){
+        solve();
+        cout<<"\n";
+        tc++;
+    }
+    return 0;
+}
+ 
