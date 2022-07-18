47a2849f5fb3a3c93f5878896da27d3d991edecc
ayushranjan26 <56040311+ayushranjan26@users.noreply.github.com>
2022-07-18 17:41:53 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1619B | Squares and Cubes/140256431.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+ 
+#define int long long int
+#define IOS std::ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define ft first
+#define sd second
+#define pii pair<int,int>
+ 
+using namespace std;
+ 
+//---------------------- PROVE, THEN WRITE ----------------------//
+ 
+const int mod = 1e9+7;
+ 
+void solve(){
+    int i,j,n;
+    cin>>n;
+    set<int> st;
+    for(i=1;i*i<=n;i++){
+        st.insert(i*i);
+    }
+    for(i=1;i*i*i<=n;i++){
+        st.insert(i*i*i);
+    }
+    cout<<st.size();
+    return;
+}
+ 
+int32_t main(){
+    //IOS;
+    int t,tc=1;
+    t=1;
+    cin>>t;
+    while(tc<=t){
+        solve();
+        cout<<"\n";
+        tc++;
+    }
+    return 0;
+}
