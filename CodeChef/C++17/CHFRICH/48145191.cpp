f83513c7b2b1fd5b9535a6028cfccc828908344c
ayushranjan26 <56040311+ayushranjan26@users.noreply.github.com>
2022-07-18 17:01:58 +0530

    Committing files

---
+++ CodeChef/C++17/CHFRICH/48145191.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+#define ll long long int
+#define pb push_back
+#define IOS std::ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define ft first
+#define sd second
+
+using namespace std;
+//---------------------- PROVE, THEN WRITE ----------------------//
+
+void solve()
+{
+    ll i,j,k,n,m,a,b,x;
+    cin>>a>>b>>x;
+    cout<<(b-a)/x;
+    return;
+}
+
+int main(){
+   // IOS;
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+        cout<<endl;
+    }
+    return 0;
+}
