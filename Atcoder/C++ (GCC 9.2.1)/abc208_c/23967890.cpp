f952345ef808ff9e9b477c8115d74c806af9b886
ayushranjan26 <56040311+ayushranjan26@users.noreply.github.com>
2022-07-18 16:59:40 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc208_c/23967890.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+#define ll long long int
+#define pb push_back
+#define IOS std::ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define ft first
+#define sd second
+
+using namespace std;
+//---------------------- PROVE, THEN WRITE ----------------------//
+
+void solve()
+{
+    ll i,j,k,a,b;
+    cin>>a>>b;
+    if(b>=a&&b<=6*a){
+        cout<<"Yes";
+    }
+    else{
+        cout<<"No";
+    }
+    return;
+}
+
+int main(){
+    //IOS;
+    int t;
+    t=1;//cin>>t;
+    while(t--){
+        solve();
+        cout<<endl;
+    }
+    return 0;
+}
+
