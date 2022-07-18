4e03af448c5e29a0c21bfcb9342ab3f35fac9832
ayushranjan26 <56040311+ayushranjan26@users.noreply.github.com>
2022-07-18 18:10:18 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1455C | Ping-pong/100032710.cpp
@@ -0,0 +1,69 @@
+/*WELCOME TO AYUSH RANJAN CODE'S*/
+ 
+#include <bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+ 
+#define IOS cin.tie(NULL);cout.tie(NULL)
+using namespace std;
+ 
+const ll mod = 1000000007;
+ 
+int chkpower2(int x){
+    return (x&&!(x&x-1));
+}
+vector<ll> primeFactors(ll n)
+{
+    vector<ll> v;
+    while (n%2 == 0)
+    {
+        v.push_back(2);
+        n = n/2;
+    }
+    for (ll i = 3; i <= sqrt(n); i = i+2)
+    {
+        while (n%i == 0)
+        {
+            v.push_back(i);
+            n = n/i;
+        }
+    }
+    v.push_back(n);
+    return v;
+}
+ 
+void solve(){
+    ll x,y;
+    cin>>x>>y;
+    x--;
+    cout<<x<<" "<<y;
+    return;
+    if(x>y){
+        x--;
+        cout<<x<<" "<<y;
+        //cout<<(x)-y/2<<" "<<0;
+        return;
+    }
+    if(y>x){
+        x--;
+        cout<<x<<" "<<y;
+        swap(x,y);
+        //cout<<0<<" "<<(x)-(y)/2;
+        return;
+    }
+    if(x==y){
+        cout<<0<<" "<<1;
+    }
+    return;
+}
+int main()
+{
+    //IOS;
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
