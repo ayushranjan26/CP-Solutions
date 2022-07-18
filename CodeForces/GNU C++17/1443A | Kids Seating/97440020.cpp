5ccc3533a0d6376fc7d11c415c8cd6c880308b3d
ayushranjan26 <56040311+ayushranjan26@users.noreply.github.com>
2022-07-18 18:11:50 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1443A | Kids Seating/97440020.cpp
@@ -0,0 +1,61 @@
+/*WELCOME TO AYUSH RANJAN CODE'S*/
+/*EVERY PROBLEM HAS A SOLUTION SOMETIMES ELEGENT OTHERWISE COMPLEX ELEGENCE*/
+ 
+#include <bits/stdc++.h>
+#define ll long long int
+#define IOS cin.tie(NULL);cout.tie(NULL)
+using namespace std;
+ 
+const int mod = 1e9 + 7;
+ 
+int chkpower2(int x){
+    return (x&&!(x&x-1));
+}
+vector<ll> primeFactors(ll n)
+{
+    vector<ll> v;
+    while (n%2 == 0)
+    {
+        v.push_back(2);
+        n = n/2;
+    }
+    for (ll i = 3; i <= sqrt(n); i = i+2)
+    {
+        while (n%i == 0)
+        {
+            v.push_back(i);
+            n = n/i;
+        }
+    }
+    v.push_back(n);
+    return v;
+}
+ 
+void solve(){
+    int n;
+    cin>>n;
+    if(n==1){
+        cout<<"4";
+        return;
+    }
+ 
+    else{
+            int d=4*n;
+        for(int i1=0;i1<n;i1++){
+            cout<<d<<" ";
+            d-=2;
+        }
+    }
+    return;
+}
+int main()
+{
+    //IOS;
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
