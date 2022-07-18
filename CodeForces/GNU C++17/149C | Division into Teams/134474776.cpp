ed38aba3b9de47a41ab5915e7fe2d410e507df62
ayushranjan26 <56040311+ayushranjan26@users.noreply.github.com>
2022-07-18 17:46:26 +0530

    Committing files

---
+++ CodeForces/GNU C++17/149C | Division into Teams/134474776.cpp
@@ -0,0 +1,49 @@
+ #include <bits/stdc++.h>
+ 
+#define int long long
+#define IOS std::ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define ft first
+#define sd second
+#define pii pair<int,int>
+ 
+using namespace std;
+ 
+//---------------------- PROVE, THEN WRITE ----------------------//
+ 
+const int mod = 1e9+7;
+ 
+void solve(){
+    int i,j,n;
+    cin>>n;
+    int a[n];
+    vector<pii> vp;
+    for(i=0;i<n;i++){
+        cin>>a[i];
+        vp.push_back({a[i],i+1});
+    }
+    sort(vp.begin(),vp.end());
+    cout<<(n+1)/2<<endl;
+    for(i=0;i<n;i+=2){
+        cout<<vp[i].sd<<" ";
+    }
+    cout<<"\n";
+    cout<<(n)/2<<endl;
+    for(i=1;i<n;i+=2){
+        cout<<vp[i].sd<<" ";
+    }
+    cout<<"\n";
+    return;
+}
+ 
+int32_t main(){
+    //IOS;
+    int t,tc=1;
+    t=1;
+    //cin>>t;
+    while(tc<=t){
+        solve();
+        cout<<"\n";
+        tc++;
+    }
+    return 0;
+}
