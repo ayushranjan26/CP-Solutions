dd5183f4d95728f3601b7553d40377f9ab4f44e1
ayushranjan26 <56040311+ayushranjan26@users.noreply.github.com>
2022-07-18 17:46:56 +0530

    Committing files

---
+++ CodeForces/GNU C++17/197A | Plate Game/134021933.cpp
@@ -0,0 +1,38 @@
+ #include <bits/stdc++.h>
+ 
+#define ll long long
+#define IOS std::ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define ft first
+#define sd second
+#define pii pair<int,int>
+ 
+using namespace std;
+ 
+//---------------------- PROVE, THEN WRITE ----------------------//
+ 
+const int mod = 1e9+7;
+ 
+void solve(){
+    int i,j,a,b,r;
+    cin>>a>>b>>r;
+    r=2*r;
+    if(r>a||r>b){
+        cout<<"Second";
+        return;
+    }
+    cout<<"First";
+    return;
+}
+ 
+int32_t main(){
+    //IOS;
+    int t,tc=1;
+    t=1;
+    //cin>>t;
+    while(tc<=t){
+        solve();
+        cout<<"\n";
+        tc++;
+    }
+    return 0;
+}
