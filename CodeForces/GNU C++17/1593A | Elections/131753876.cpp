ff62d201acf27b2276a7247449920815873f3ce1
ayushranjan26 <56040311+ayushranjan26@users.noreply.github.com>
2022-07-18 17:48:55 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1593A | Elections/131753876.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+ 
+#define int long long
+#define IOS std::ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define ft first
+#define sd second
+#define pii pair<int,int>
+ 
+using namespace std;
+ 
+//---------------------- PROVE, THEN WRITE ----------------------//
+ 
+const int mod = 1e9+7;
+ 
+void solve(){
+    int i,j,k,a,b,c;
+    cin>>a>>b>>c;
+    cout<<max(max(b,c)-a+1,0LL)<<" "<<max(max(a,c)-b+1,0LL)<<" "<<max(max(b,a)-c+1,0LL);
+ 
+    return;
+}
+ 
+int32_t main(){
+    //IOS;
+    int t,tc=1;
+    t=1;
+    cin>>t;
+    while(tc<=t){
+        solve();
+        cout<<"\n";
+        tc++;
+    }
+    return 0;
+}
