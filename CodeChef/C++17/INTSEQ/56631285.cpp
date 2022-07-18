2a4aaeb56dd5e009f58a581f2a9fa739d3a0008d
ayushranjan26 <56040311+ayushranjan26@users.noreply.github.com>
2022-07-18 17:01:15 +0530

    Committing files

---
+++ CodeChef/C++17/INTSEQ/56631285.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+#define ll long long int
+#define IOS std::ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define ft first
+#define sd second
+#define pii pair<int,int>
+
+using namespace std;
+
+//---------------------- PROVE, THEN WRITE ----------------------//
+
+const int mod = 1e9+7;
+
+void solve(){
+    int i,j,k;
+    cin>>k;
+    int res=0;
+    while(k>0&&k%2==0){
+        k/=2;
+        res++;
+    }
+    cout<<res;
+    return;
+}
+
+int32_t main(){
+    //IOS;
+    int t,tc=1;
+    t=1;
+    cin>>t;
+    while(tc<=t){
+        solve();
+        cout<<"\n";
+        tc++;
+    }
+    return 0;
+}
+
