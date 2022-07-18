0fdee5d0aa0cfac36b123dc9e14e4aea8471c326
ayushranjan26 <56040311+ayushranjan26@users.noreply.github.com>
2022-07-18 18:01:32 +0530

    Committing files

---
+++ CodeForces/GNU C++17/545D | Queue/111832624.cpp
@@ -0,0 +1,49 @@
+/*WELCOME TO AYUSH RANJAN CODE*/
+ 
+#include <bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define IOS std::ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define all(x) (x).begin(), (x).end()
+#define loop(i,a,b) for(int i=(a);i<(b);i++)
+#define rloop(i,a,b) for(int i=(a);i>(b);i--)
+#define ft first
+#define sd second
+ 
+using namespace std;
+ 
+ll const INF =1e18;
+ 
+ 
+void solve()
+{
+    ll i,j,x,y,z,n,m,ans=0;
+    cin>>n;
+    ll a[n];
+    for(i=0;i<n;i++){
+        cin>>a[i];
+    }
+    sort(a,a+n);
+    ll tm=0;
+    for(i=0;i<n;i++){
+        if(a[i]>=tm){
+            ans++;
+            tm+=a[i];
+        }
+    }
+    cout<<ans;
+    return ;
+}
+ 
+int main()
+{
+    //IOS;
+    ll t;
+    t=1;//cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
