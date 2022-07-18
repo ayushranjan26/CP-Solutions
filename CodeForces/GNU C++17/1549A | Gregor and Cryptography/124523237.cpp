51f8fe82b5d7a33901b8699403256a39db11916c
ayushranjan26 <56040311+ayushranjan26@users.noreply.github.com>
2022-07-18 17:51:46 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1549A | Gregor and Cryptography/124523237.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+ 
+#define ll long long int
+#define pb push_back
+#define IOS std::ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define ft first
+#define sd second
+ 
+using namespace std;
+ 
+//---------------------- PROVE, THEN WRITE ----------------------//
+ll mod=1e9+7;
+ 
+void solve(){
+    int i,j,k,n;
+    cin>>n;
+    cout<<(n-1)/2<<" "<<(n-1);
+    return;
+}
+ 
+int main(){
+    //IOS;
+    int t,tc=1;
+    t=1;
+    cin>>t;
+    while(tc<=t){
+        solve();
+        cout<<"\n";
+        tc++;
+    }
+    return 0;
+}
+ 
+ 
