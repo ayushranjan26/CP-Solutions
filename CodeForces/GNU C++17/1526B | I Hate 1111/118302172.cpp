d669757f7054b462d85bd9171691b506ce66f04c
ayushranjan26 <56040311+ayushranjan26@users.noreply.github.com>
2022-07-18 17:58:26 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1526B | I Hate 1111/118302172.cpp
@@ -0,0 +1,47 @@
+/*WELCOME TO AYUSH RANJAN CODE*/
+ 
+#include <bits/stdc++.h>
+ 
+#define ll long long int
+#define pb push_back
+#define IOS std::ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define all(x) (x).begin(), (x).end()
+#define loop(i,a,b) for(int i=(a);i<(b);i++)
+#define rloop(i,a,b) for(int i=(a);i>(b);i--)
+#define ft first
+#define sd second
+ 
+using namespace std;
+ 
+ll const INF =1e18;
+ll M =1000000007;
+ 
+void solve()
+{
+    ll i,j,n,m,x;
+    cin>>x;
+    i=0;
+    while(x>=0&&i<15){
+        if(x%11==0){
+            cout<<"YES";
+            return;
+        }
+        x-=111;
+        i++;
+    }
+    cout<<"NO";
+    return;
+}
+ 
+int main()
+{
+    //IOS;
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+        cout<<endl;
+    }
+    return 0;
+}
