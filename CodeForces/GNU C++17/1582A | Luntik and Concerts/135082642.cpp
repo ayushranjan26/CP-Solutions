ced2da5793dad37b93ba5910fb220e65a704a23d
ayushranjan26 <56040311+ayushranjan26@users.noreply.github.com>
2022-07-18 17:46:02 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1582A | Luntik and Concerts/135082642.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+ 
+#define int long long
+#define IOS std::ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define ft first
+#define sd second
+#define pii pair<int,int>
+ 
+using namespace std;
+ 
+//---------------------- PROVE, THEN WRITE ----------------------//
+ 
+const int mod = 1e9+7;
+ 
+void solve(){
+    int i,a,b,c;
+    cin>>a>>b>>c;
+    if((a+2*b+3*c)%2==1){
+        cout<<"1";
+    }
+    else{
+        cout<<"0";
+    }
+    return;
+}
+ 
+int32_t main(){
+    //IOS;
+    int t,tc=1;
+    t=1;
+    cin>>t;
+    while(tc<=t){
+        solve();
+        cout<<"\n";
+        tc++;
+    }
+    return 0;
+}
