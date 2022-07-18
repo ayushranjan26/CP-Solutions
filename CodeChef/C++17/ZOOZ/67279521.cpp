a636f3cc279a4cb855ed197344c09c95d1ddbd1d
ayushranjan26 <56040311+ayushranjan26@users.noreply.github.com>
2022-07-18 17:03:22 +0530

    Committing files

---
+++ CodeChef/C++17/ZOOZ/67279521.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+#define int long long int
+#define IOS std::ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define ft first
+#define sd second
+#define pii pair<int,int>
+
+using namespace std;
+
+const int mod = 1e9+7;
+
+void solve(){
+    int i,j,n;
+    cin>>n;
+    string s="";
+    for(i=0;i<n;i++){
+        s+='0';
+    }
+    s[0]='1';s[n-1]='1';
+    cout<<s;
+    return;
+}
+
+int32_t main(){
+    //IOS;
+    int t,tc=1;
+    t=1;
+    cin>>t;
+    while(tc<=t){
+        solve();
+        cout<<"\n";
+        tc++;
+    }
+    return 0;
+}
