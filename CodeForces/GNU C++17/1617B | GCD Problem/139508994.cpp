6ed6a8a79ae61dc0e8bb29702922ddf811904adf
ayushranjan26 <56040311+ayushranjan26@users.noreply.github.com>
2022-07-18 17:42:22 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1617B | GCD Problem/139508994.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+ 
+#define int long long int
+#define IOS std::ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define ft first
+#define sd second
+#define pii pair<int,int>
+ 
+using namespace std;
+ 
+//---------------------- PROVE, THEN WRITE ----------------------//
+ 
+const int mod = 1e9+7;
+ 
+void solve(){
+    int i,j,k,n;
+    cin>>n;
+    int a,b,c;
+    if(n%2==0){
+        c=1;
+        a=(n-1)/2;
+        b=a+1;
+    }
+    else{
+        c=1;
+        n--;
+        i=3;
+        while(1){
+            if(__gcd(i,n-i)==c){
+                break;
+            }
+            i++;
+        }
+        a=i;
+        b=n-i;
+    }
+    cout<<a<<" "<<b<<" "<<c<<endl;
+    return;
+}
+ 
+int32_t main(){
+    //IOS;
+    int t,tc=1;
+    t=1;
+    cin>>t;
+    while(tc<=t){
+        solve();
+        cout<<"\n\n";
+        tc++;
+    }
+    return 0;
+}
