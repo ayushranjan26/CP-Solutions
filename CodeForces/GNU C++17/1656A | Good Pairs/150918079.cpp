605891d16440ec4e960ec75d4b5baa4df3f31998
ayushranjan26 <56040311+ayushranjan26@users.noreply.github.com>
2022-07-18 17:35:44 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1656A | Good Pairs/150918079.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+ 
+#define int long long int
+#define IOS std::ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define IOS std::ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define ft first
+#define sd second
+#define pii pair<int,int>
+ 
+using namespace std;
+ 
+//---------------------- PROVE, THEN WRITE ----------------------//
+ 
+const int mod = 1e9+7;
+ 
+void solve(){
+    int i,j,n;
+    cin>>n;
+    vector<pii> vp(n);
+    for(i=0;i<n;i++){
+        cin>>vp[i].ft;
+        vp[i].sd=i+1;
+    }
+    sort(vp.begin(),vp.end());
+    cout<<vp[0].sd<<" "<<vp[n-1].sd;
+    return;
+}
+ 
+int32_t main(){
+    //IOS;
+    int t,tc=1;
+    t=1;
+    cin>>t;
+    while(tc<=t){
+        solve();
+        cout<<"\n\n";
+        tc++;
+    }
+    return 0;
+}
+ 
