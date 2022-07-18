2c89bfeeb45bab58199ec5154bcbf769407c3f36
ayushranjan26 <56040311+ayushranjan26@users.noreply.github.com>
2022-07-18 17:00:09 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc171_d/14568942.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    char c1;
+    cin>>c1;
+    int d1=(int)c1;
+   // cout<<d1;
+    if(d1<97){
+        cout<<"A";
+    }
+    if(d1>=97){
+        cout<<"a";
+    }
+    return 0;
+}
