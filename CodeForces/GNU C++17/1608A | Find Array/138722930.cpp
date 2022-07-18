f6e1ee87ad2228370edb29c4bfbaa4ceefc58fcf
ayushranjan26 <56040311+ayushranjan26@users.noreply.github.com>
2022-07-18 17:43:34 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1608A | Find Array/138722930.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+ 
+#define int long long int
+#define IOS std::ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define ft first
+#define sd second
+#define pii pair<int,int>
+ 
+using namespace std;
+ 
+//---------------------- PROVE, THEN WRITE ----------------------//
+ 
+const int mod = 1e9+7;
+ 
+void solve(){
+    int i,j,n;
+    cin>>n;
+    for(i=0;i<n;i++){
+        cout<<i+2<<" ";
+    }
+    return;
+}
+ 
+int32_t main(){
+    //IOS;
+    int t,tc=1;
+    t=1;
+    cin>>t;
+    while(tc<=t){
+        solve();
+        cout<<"\n";
+        tc++;
+    }
+    return 0;
+}
