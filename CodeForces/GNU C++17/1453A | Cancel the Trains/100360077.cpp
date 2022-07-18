30707227c563fb172cd95e9454eda3438007f098
ayushranjan26 <56040311+ayushranjan26@users.noreply.github.com>
2022-07-18 18:10:03 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1453A | Cancel the Trains/100360077.cpp
@@ -0,0 +1,67 @@
+/*WELCOME TO AYUSH RANJAN CODE'S*/
+ 
+#include <bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+ 
+#define IOS cin.tie(NULL);cout.tie(NULL)
+using namespace std;
+ 
+const ll mod = 1000000007;
+ 
+int chkpower2(int x){
+    return (x&&!(x&x-1));
+}
+vector<ll> primeFactors(ll n)
+{
+    vector<ll> v;
+    while (n%2 == 0)
+    {
+        v.push_back(2);
+        n = n/2;
+    }
+    for (ll i = 3; i <= sqrt(n); i = i+2)
+    {
+        while (n%i == 0)
+        {
+            v.push_back(i);
+            n = n/i;
+        }
+    }
+    v.push_back(n);
+    return v;
+}
+ 
+void solve(){
+    int n,i,j,m;
+    cin>>n>>m;
+    set<int> s;
+    for(i=0;i<n;i++){
+        int d;
+        cin>>d;
+        s.insert(d);
+    }
+    int ans=0;
+    for(i=0;i<m;i++){
+        int d;
+        cin>>d;
+        if(s.find(d)!=s.end()){
+            ans++;
+        }
+    }
+ 
+ 
+    cout<<ans;
+    return;
+}
+int main()
+{
+    //IOS;
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+        cout<<"\n";
+    }
+    return 0;
+}
