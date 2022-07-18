a42af0806e708716be3cf331913581649371e775
ayushranjan26 <56040311+ayushranjan26@users.noreply.github.com>
2022-07-18 17:36:32 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1629C | Meximum Array/144048629.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+ 
+#define int long long int
+#define IOS std::ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define ft first
+#define sd second
+#define pii pair<int,int>
+ 
+using namespace std;
+ 
+//---------------------- PROVE, THEN WRITE ----------------------//
+ 
+const int mod = 1e9+7;
+ 
+void solve(){
+    int i,j,n;
+    cin>>n;
+    int arr[n];
+    set<int> mp[n+5];
+    for(i=0;i<n;i++){
+        cin>>arr[i];
+        if(arr[i]<n+4){
+            mp[arr[i]].insert(i);
+        }
+    }
+ 
+    int mn=-1;
+    vector<int> res;
+    while(mn<n-1){
+        int mnn=mn;
+        for(i=0;i<=n+1;i++){
+            while((!mp[i].empty())&&(mn >= *mp[i].begin())){
+                mp[i].erase(*mp[i].begin());
+            }
+            if(mp[i].size()==0){
+                res.push_back(i);
+                break;
+            }
+            else{
+                mnn=max(mnn,*mp[i].begin());
+            }
+        }
+        if(res.back()==0){
+            mn++;
+        }
+        mn=max(mnn,mn);
+    }
+ 
+    cout<<res.size()<<endl;
+    for(i=0;i<res.size();i++){
+        cout<<res[i]<<" ";
+    }
+    return;
+}
+ 
+int32_t main(){
+    //IOS;
+    int t,tc=1;
+    t=1;
+    cin>>t;
+    while(tc<=t){
+        solve();
+        cout<<"\n\n";
+        tc++;
+    }
+    return 0;
+}
