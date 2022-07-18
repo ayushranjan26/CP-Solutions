904ac2b24075ec63cb5c601820c1a8727e88a5ab
ayushranjan26 <56040311+ayushranjan26@users.noreply.github.com>
2022-07-18 17:01:06 +0530

    Committing files

---
+++ CodeChef/C++17/PREFPERM/58794298.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+#define int long long int
+#define IOS std::ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define ft first
+#define sd second
+#define pii pair<int,int>
+
+using namespace std;
+
+//---------------------- PROVE, THEN WRITE ----------------------//
+
+const int mod = 1e9+7;
+
+void solve(){
+    int i,j,n,k;
+    cin>>n>>k;
+    int a[k];
+    for(i=0;i<k;i++){
+        cin>>a[i];
+    }
+    sort(a,a+k);
+    vector<int> res;
+    set<int> st;
+    for(i=0;i<n;i++){
+        st.insert(i+1);
+    }
+    for(i=0;i<k;i++){
+        st.erase(a[i]);
+    }
+
+    for(i=0;i<k;i++){
+        res.push_back(a[i]);
+        while((!st.empty())&&((*st.begin())<a[i])){
+            res.push_back(*st.begin());
+            st.erase(st.begin());
+        }
+    }
+    for(i=0;i<n;i++){
+        cout<<res[i]<<" ";
+    }
+    return;
+}
+
+int32_t main(){
+    IOS;
+    int t,tc=1;
+    t=1;
+    cin>>t;
+    while(tc<=t){
+        solve();
+        cout<<"\n";
+        tc++;
+    }
+    return 0;
+}
